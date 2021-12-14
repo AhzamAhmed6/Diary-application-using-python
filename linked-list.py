6b0631f50ba963bef922502b2c61c8d4849f4b33
Ahzam <ahzamahmed6@gmail.com>
2021-12-13 23:34:11 -0800

    create the add_after function

--- linked-list.py
+++ linked-list.py
@@ -11,18 +11,18 @@ class LinkedList:
     '''This class is for performing the operations to the node which we create using class Node'''
 
     def __init__(self):
-        self.head = None        # we are creating an empty linked list (remember we are not creating a node, we are creating linked list, for creating node we already have method above)
+        self.head = None           # we are creating an empty linked list (remember we are not creating a node, we are creating linked list, for creating node we already have method above)
 
     def print_LL(self):
         '''This methord is for traversing Linked List'''
 
-        if self.head == None:    # if the linked list is empty
+        if self.head == None:      # if the linked list is empty
             print('The linked list is empty')
 
         else:
             n = self.head
             while n is not None:
-                print(n.data)
+                print(n.data, '--->', end=' ')
                 n = n.ref
 
     def add_begin(self, data):
@@ -40,8 +40,25 @@ class LinkedList:
                 n = n.ref         # to reach to the last node
             n.ref = new_node      # set the refrence of last node to the new node
 
+    def add_after(self, data, x):
+        n = self.head
+        while n is not None:      # first we find the node after which we wants to add the new_node
+            if x==n.data:         # if we found the new node then we set the node name as n and break the loop
+                break    
+            n = n.ref             # else we are constantly updating the current node until we reach the required node
+        if n is None:             # if n is still empty after traversing the whole linked list, it's mean the node is not present
+            print('The linked list is empty')
+        else:
+            new_node = Node(data) # if we found the required node, only then we create new node
+            new_node.ref = n.ref  # set the refrence of new node to the next node (the addres of next node is initially stored in the n.ref)
+            n.ref = new_node      # set the refrence of perivous node to the new_node
+            
+
 LL1 = LinkedList()
 LL1.add_begin('ahzam')
+LL1.add_end('ahmed')
+LL1.add_begin('my name is')
+LL1.add_end('yes this is my name')
 LL1.print_LL()
 
         # self.head is just store the refrence of first node 
