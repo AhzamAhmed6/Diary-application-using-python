24f5377189c0318011a75ce7caa2860fbc556b85
Ahzam <ahzamahmed6@gmail.com>
2021-12-13 22:28:29 -0800

    create add_begin function

--- linked-list.py
+++ linked-list.py
@@ -1,3 +1,5 @@
+
+
 class Node:
     '''This class is just for creating a Node, thats why we set the refrence field to Null'''
     
@@ -5,12 +7,11 @@ class Node:
         self.data = data
         self.ref = None
 
-
 class LinkedList:
     '''This class is for linked the nodes which we create using class Node'''
 
     def __init__(self):
-        self.head = None  # we are creating an empty linked list
+        self.head = None        # we are creating an empty linked list
 
     def print_LL(self):
         '''This methord is for traversing Linked List'''
@@ -23,6 +24,12 @@ class LinkedList:
             while n is not None:
                 print(n.data)
                 n = n.ref
+
+    def add_begin(self, data):
+        new_node = Node(data)     # we create new node with data=data  and  ref=Null
+        new_node.ref = self.head  # we set the refrence of new node to the head
+        self.head = new_node      # set the head to the new node
+
 
 
 
