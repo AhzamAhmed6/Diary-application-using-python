13d409ee8413e071b5efe03ff12bac3e5c6bf2b6
Ahzam <ahzamahmed6@gmail.com>
2021-12-13 20:58:25 -0800

    create class Node

---
+++ linked-list.py
@@ -0,0 +1,6 @@
+class Node:
+    '''This class is just for creating a Node, thats why we set the refrence field to 
+    Null'''
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
