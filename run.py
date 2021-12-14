e1f2268cb54af162da8c1b3e7b89dcd20edd0f97
Ahzam <ahzamahmed6@gmail.com>
2021-12-14 08:48:48 -0800

    create 5 pages for diary

---
+++ run.py
@@ -0,0 +1,9 @@
+from LinkedList import *
+import sys
+
+
+path = r'C:\Users\Lenovo\Downloads\visual stdio programs\Python_Programs\Data-Structure-Project-2\pages'
+for i in range(100):
+    open(path+'\'+i+'.txt', 'rw')
+
+
