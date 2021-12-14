ca1dcd19012a44b6b1676daaf153b495c01bf02b
Ahzam <ahzamahmed6@gmail.com>
2021-12-14 11:28:37 -0800

    successfully implement the move_next_node function in to our run.py script

--- run.py
+++ run.py
@@ -17,20 +17,20 @@ if page_number>5:
     print('Page limit reached')
 access = input('Read(r), Write(w) : ')
 
+
+'''For read only option'''
+
 if access == 'r':
-    file = open(f'{path}\\{page_number}.txt', 'r')
+    p = f'{path}\\{page_number}.txt'
+    file = open(p, 'r')
     print(file.read())
 
     while True:        
             next_page = input('Do you wants to go to next page (y/n): ')
             if next_page == 'y':
-                page_number = page_number+1
-                if page_number>5:
-                    print('Page limit reached')
-                    break
-                else:
-                    file = open(f'{path}\\{page_number}.txt', 'r')
-                    print(file.read())
+                p = diary.move_next_node(p)
+                file = open(p, 'r')
+                print(file.read())
             elif next_page == 'n':
                 break
             else:
