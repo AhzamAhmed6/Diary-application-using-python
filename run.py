d21432a05e166742f7e367c684f90d6067152c19
Ahzam <ahzamahmed6@gmail.com>
2021-12-16 06:13:47 -0800

    create recursive function mydiary()

--- run.py
+++ run.py
@@ -12,26 +12,50 @@ diary.add_after(path+'\\5.txt', path+'\\4.txt')
 diary.print_LL()
 
 
-page_number = int(input('Enter the page number: '))
-if page_number>5:
-    print('Page limit reached')
-access = input('Read(r), Write(w) : ')
+def mydiary(access):
+    page_number = int(input('Enter the page number: '))
+    if page_number>5:
+        print('Page limit reached')
 
 
-'''For read only option'''
 
-if access == 'r':
-    p = f'{path}\\{page_number}.txt'
-    file = open(p, 'r')
-    print(file.read())
+    '''For read only option'''
 
-    while True:        
-            next_page = input('Do you wants to go to next page (y/n): ')
-            if next_page == 'y':
-                p = diary.move_next_node(p)
-                file = open(p, 'r')
-                print(file.read())
-            elif next_page == 'n':
-                break
-            else:
-                print('Please chose valid option')
+    if access == 'r':
+        p = f'{path}\\{page_number}.txt'
+        file = open(p, 'r')
+        print(file.read())
+        while True:        
+                next_page = input('Do you wants to go to next page (y/n): ')
+                if next_page == 'y':
+                    mydiary(access=access)
+                    # print(file.read())
+                elif next_page == 'n':
+                    return
+                else:
+                    print('Please chose valid option')
+
+        file.close()
+
+
+    if access == 'w':
+        p = f'{path}\\{page_number}.txt'
+        file = open(p, 'w')
+        text = input(f'Input your text for page no {page_number}: ')
+        file.write(text)
+
+        while True:        
+                next_page = input('Do you wants to go to next page (y/n): ')
+                if next_page == 'y':
+                    p = diary.move_next_node(p)
+                    file = open(p, 'r')
+                    print(file.read())
+                elif next_page == 'n':
+                    break
+                else:
+                    print('Please chose valid option')
+
+        file.close()
+
+access = input('Read(r), Write(w), Append(a) : ')
+mydiary(access=access)
