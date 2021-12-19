7b910e2fc31210fb4cec17610a05b704dfffb3ce
Ahzam <ahzamahmed6@gmail.com>
2021-12-18 22:04:43 -0800

    init commit

--- run.py
+++ run.py
@@ -1,27 +1,29 @@
 from LinkedList import *
 from tkinter import *
 import subprocess
+import os
 
-def create_pages(pages, path):
+def create_pages(path, pages=10):
     diary = LinkedList()
     diary.add_begin(path+fr'\1.txt')
-    open(path+r'\1.txt', 'w')
+    # open(path+r'\1.txt', 'w')/
     for i in range(2,pages+1):
         diary.add_after(path+fr'\{i}.txt', path+fr'\{i-1}.txt')
-        open(path+fr'\{i}.txt', 'w')
+        # open(path+fr'\{i}.txt', 'w')
     return diary
 
+
 page_number = int(input('Enter the page number: '))
-def myClick(page_number=page_number):
-    p = fr'{path}\{page_number}.txt'
-    for i in range(3):
-        subprocess.call(['notepad.exe', p])
+path = r'C:\Users\Lenovo\Downloads\visual_stdio_programs\Python_Programs\Data-Structure-Project-2\pages'
+diary = create_pages(path, 10)
+
+p = fr'{path}\{page_number}.txt'
+os.system(p)
+while True:    
+    next_page = input('Next Page?\nYes(y),   No(n)  : ')
+    if next_page == 'y':
         p = diary.move_next_node(p)
-
-path = r'C:\Users\Lenovo\Downloads\visual stdio programs\Python_Programs\Data-Structure-Project-2\pages'
-diary = create_pages(10, path)
-
-root = Tk()
-myButton = Button(root, text = 'Next Page', command=myClick)
-myButton.pack()
-root.mainloop()
+        # p = fr'{path}\{page_number}.txt'
+        os.system(p)       
+    else:
+        break
