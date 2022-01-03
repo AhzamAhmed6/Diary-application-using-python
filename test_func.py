6afbe6fd8ec4d83a495d28256a0e4091a76ba976
Ahzam <ahzamahmed6@gmail.com>
2022-01-03 13:01:10 -0800

    perform pytest Perform pytest on one step backward function

--- test_func.py
+++ test_func.py
@@ -9,3 +9,11 @@ def test_one_step_forward():
     a = mynode.head.data
     b = mynode.head.nref.data
     assert mynode.one_step_forward(x = str(a)) == str(b)
+
+def test_one_step_backward():
+    mynode = Doubly_LL()
+    mynode.add_begin('ahzam')
+    mynode.add_begin('ahmed')
+    a = mynode.head.data
+    b = mynode.head.nref.data
+    assert mynode.one_step_backward(x = str(b)) == str(a)
